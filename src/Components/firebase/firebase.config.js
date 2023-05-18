501d6aef55e2cc8ea14dd94e646f6fd479f8b841
Habibaferdausi <ritu.pramanik1@gmail.com>
2023-05-18 12:02:35 +0600

    add firebase

---
+++ src/Components/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyARg23QNt5c3Piaw5j3Ak2-CsypZvG6khE",
+  authDomain: "zoo-zone-toys.firebaseapp.com",
+  projectId: "zoo-zone-toys",
+  storageBucket: "zoo-zone-toys.appspot.com",
+  messagingSenderId: "360380770655",
+  appId: "1:360380770655:web:cb7d2bc5f8d7dfeeb2aeea",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
